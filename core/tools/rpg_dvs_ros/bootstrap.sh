76d54e27e1cb576f9e4986ab89c131647b9969c8
discoimp <georgbe@gmail.com>
2023-02-08 21:57:53 +0100

    added libcaer

--- core/tools/rpg_dvs_ros/bootstrap.sh
+++ core/tools/rpg_dvs_ros/bootstrap.sh
@@ -8,4 +8,12 @@ echo "Installing IniVation DVXplorer dependencies."
 export ROS_DISTRO=noetic    
 apt-get update && apt-get install -y --no-install-recommends ros-${ROS_DISTRO}-camera-info-manager ros-${ROS_DISTRO}-image-view python-catkin-tools git software-properties-common
 
+# install IniVation dependencies
+# run update again after software-properties-common
+apt-get update
+add-apt-repository ppa:inivation-ppa/inivation
+apt-get update && apt-get install -y dv-runtime
+
+# Install libcaer after dv-runtime is installed.
+apt-get install -y libcaer-dev
 
