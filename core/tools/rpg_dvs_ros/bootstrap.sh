6e82f72398d4a577b16363d470f637a85d6a5a7c
discoimp <georgbe@gmail.com>
2023-02-08 21:33:55 +0100

    removed old stuff from bootstrap.sh

--- core/tools/rpg_dvs_ros/bootstrap.sh
+++ core/tools/rpg_dvs_ros/bootstrap.sh
@@ -4,6 +4,8 @@
 set -e
 
 # Dnsmasq provides DHCP and DNS servers for small networks
-echo "Installing dnsmasq."
-apt update
-apt install -y --no-install-recommends dnsmasq
+echo "Installing IniVation DVXplorer dependencies."
+export ROS_DISTRO=noetic    
+apt-get update && apt-get install -y --no-install-recommends ros-${ROS_DISTRO}-camera-info-manager ros-${ROS_DISTRO}-image-view python-catkin-tools git software-properties-common
+
+
