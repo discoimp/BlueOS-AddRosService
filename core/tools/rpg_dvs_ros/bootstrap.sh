7c72a0718f4642c50dc9c89a6bbb15656f8ab438
discoimp <georgbe@gmail.com>
2023-02-09 09:43:45 +0100

    add docker

--- core/tools/rpg_dvs_ros/bootstrap.sh
+++ core/tools/rpg_dvs_ros/bootstrap.sh
@@ -3,7 +3,7 @@
 # Exit if something goes wrong
 set -e
 
-# Dnsmasq provides DHCP and DNS servers for small networks
+# Install basic tools
 echo "Installing IniVation DVXplorer dependencies."
 export ROS_DISTRO=noetic    
 apt-get update && apt-get install -y --no-install-recommends ros-${ROS_DISTRO}-camera-info-manager ros-${ROS_DISTRO}-image-view python-catkin-tools git software-properties-common
